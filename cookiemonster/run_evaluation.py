Fix off by one error in microbench date gen

cookiemonster/run_evaluation.py
@@ -279,15 +279,17 @@ class Evaluation:
                     kappa = self.config.user.bias_detection_knob
 
                     mlflow.log_metrics(
-                        {
+                        metrics={
                             f"true_output": true_output[1],
                             f"aggregation_output": aggregation_output[1],
                             f"aggregation_noisy_output": aggregation_noisy_output[1],
-                            f"true_bias": aggregation_output[1] - true_output[1],
+                            f"true_bias": true_output[1] - aggregation_output[1],
                             "true_empty_epochs": true_output[0] / kappa,
                             "noisy_empty_epochs": aggregation_noisy_output[0] / kappa,
+                            "noisy_bias": aggregation_noisy_output[0] * (batch.global_sensitivity / kappa),
                             "noisy_bias_bound": (aggregation_noisy_output[0] + batch.noise_scale * np.log(1/0.05) / np.sqrt(2)  ) * (batch.global_sensitivity / kappa),
-                        }
+                        },
+                        step = self.num_queries_answered
                     )
 
                 elif isinstance(true_output, np.ndarray):
@@ -301,7 +303,9 @@ class Evaluation:
                                 f"aggregation_noisy_output_{i}": aggregation_noisy_output[
                                     i
                                 ],
-                            }
+                            },
+                            step = self.num_queries_answered
+
                         )
 
                 else:
@@ -310,7 +314,8 @@ class Evaluation:
                             "true_output": true_output,
                             "aggregation_output": aggregation_output,
                             "aggregation_noisy_output": aggregation_noisy_output,
-                        }
+                        },
+                        step = self.num_queries_answered
                     )
 
     def _log_all_filters_state(self):

data/microbenchmark/create_dataset.py
@@ -15,7 +15,8 @@ def generate_random_dates(start_date, num_days, num_samples):
     # start_seconds = 0
     start_seconds = int((start_date - datetime.datetime(1969, 12, 31)).total_seconds())
     # end_seconds = int((end_date - datetime.datetime(1969, 12, 31)).total_seconds())
-    end_seconds = start_seconds + (num_days * 24 * 60 * 60)
+    end_seconds = start_seconds + (num_days * 24 * 60 * 60) + 1
+        
     random_seconds = np.random.randint(
         start_seconds, end_seconds, size=num_samples, dtype=int
     )
@@ -47,7 +48,7 @@ def generate_impressions(start_date, num_days, config, publisher_user_profile):
 
 def generate_conversions(product_id, publisher_user_profile, advertiser_id, config):
     start_date = datetime.datetime(2024, 1, 31)
-    num_days = config.num_days - 31
+    num_days = config.num_days - 31 # TODO: why is that?
 
     publisher_user_profile["means"] = 5
     num_converted_users = int(